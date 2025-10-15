9097a7743d36d54b46b697e4a36623b4b692f1df
Hhgffddetfdsssszv <102561391+Hhgffddetfdsssszv@users.noreply.github.com>
2025-10-15 17:37:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 用户id
-const userId = process.env.muserId || ""
+const userId = process.env.muserId || "17631554857"
 // 用户token 可以使用网页登录获取
-const token = process.env.mtoken || ""
+const token = process.env.mtoken || "STnid000001176052095187304S9CEoKeeucCtuQXohrVF7dpvjfuvdV"
 // 本地运行端口号
 const port = process.env.mport || 1234
 // 访问地址，用于epg和节目访问。
@@ -11,6 +11,6 @@ const host = process.env.mhost || "http://localhost:1234"
 // 4蓝光(需要登录且账号有VIP)
 // 3高清
 // 2标清
-const rateType = process.env.mrateType || 3
+const rateType = process.env.mrateType || 4
 
 export { userId, token, port, host, rateType }
